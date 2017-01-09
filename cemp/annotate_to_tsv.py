5e4713e48f6e3a12770b98cdcf5aee2507025096
LLCampos <luis1212@sapo.pt>
2017-01-09 15:43:42 +0000

    improve CEMP TSV file generator

--- cemp/annotate_to_tsv.py
+++ cemp/annotate_to_tsv.py
@@ -16,10 +16,11 @@ def escape_special_characters(string):
 
 
 set_to_annotate = sys.argv[1]
+data_source = sys.argv[2]
 
 becalm_tsv_header = 'DOCUMENT_ID\tSECTION\tINIT\tEND\tSCORE\tANNOTATED_TEXT\tTYPE\tDATABASE_ID\n'
 
-annotatons_file = open('{}_set_annots.tsv'.format(set_to_annotate), 'w')
+annotatons_file = open('{}_{}_set_annots.tsv'.format(data_source, set_to_annotate), 'w')
 annotatons_file.write(becalm_tsv_header)
 
 with open('{}_set.txt'.format(set_to_annotate)) as f:
@@ -31,8 +32,12 @@ for document in documents:
     abstract = escape_special_characters(abstract)
     text = escape_special_characters(text)
 
-    annot_abstract_commant = 'cd ..; bash get_entities.sh {} {} "{}"'.format(document_id, 'A', abstract)
-    annot_text_commant = 'cd ..; bash get_entities.sh {} {} "{}"'.format(document_id, 'T', text)
+    annot_abstract_commant = 'cd ..; bash get_entities.sh {} {} "{}" {}'.format(
+        document_id, 'A', abstract, data_source
+    )
+    annot_text_commant = 'cd ..; bash get_entities.sh {} {} "{}" {}'.format(
+        document_id, 'T', text, data_source
+    )
 
     annotatons_file.write(subprocess.check_output(annot_abstract_commant, shell=True))
     annotatons_file.write(subprocess.check_output(annot_text_commant, shell=True))
