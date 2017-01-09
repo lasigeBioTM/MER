9324ebb1a7b962a21b5b0523cd9f9918772fc704
LLCampos <luis1212@sapo.pt>
2017-01-09 20:29:44 +0000

    correct test and add test related to issue #9

--- tests/tests.py
+++ tests/tests.py
@@ -18,10 +18,16 @@ class SanityCheckTests(unittest.TestCase):
     #     self.assertEqual(result, sample_result)
 
     def test_no_annotations(self):
-        bash_command = 'bash get_entities.sh 1 T "not a entity"'
+        bash_command = 'bash get_entities.sh 1 T "not a entity" DrugBank'
         result = subprocess.check_output(bash_command, shell=True)
 
         self.assertEqual('', result)
+
+    def test_no_overlapping_annotations(self):
+        bash_command = 'bash get_entities.sh 1 T "Benzalkonium Chloride" ChEBI'
+        result = subprocess.check_output(bash_command, shell=True)
+
+        self.assertEqual('1\tT\t0\t21\t0.671541\tBenzalkonium Chloride\tunknown\t1', result)
 
 
 if __name__ == '__main__':
