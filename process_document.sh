766852922ee233737c898eef2182441dba3dac8c
AndreLamurias <alamurias@lasige.di.fc.ul.pt>
2017-01-19 17:14:14 +0000

    Use ts to call process_document

--- process_document.sh
+++ process_document.sh
@@ -5,9 +5,9 @@ declare source=$2
 SAVEIFS=$IFS; IFS=$(echo -en "");
 if [ $source = "PUBMED" ]; then
     declare title=$(./external_services/pubmed.sh $docid 2>/dev/null| jq '.title')
-    #declare abstract=$(./external_services/pubmed.sh $docid 2>/dev/null| jq '.abstract')
+    declare abstract=$(./external_services/pubmed.sh $docid 2>/dev/null| jq '.abstract')
     declare titleresults=$(./get_entities.sh $docid "T" "$title" "ChEBI")
-    #declare abstractresults=$(./get_entities.sh $docid "A" $abstract "ChEBI")
-    echo $titleresults #$abstractresults
+    declare abstractresults=$(./get_entities.sh $docid "A" $abstract "ChEBI")
+    echo $titleresults $abstractresults
 fi
 
