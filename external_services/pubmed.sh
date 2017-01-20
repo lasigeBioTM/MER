c2a6eab9e0ba84df7983b3632f67696270aeed33
LLCampos <luis1212@sapo.pt>
2017-01-20 10:34:41 +0000

    add bash request support for pubmed client

--- external_services/pubmed.sh
+++ external_services/pubmed.sh
@@ -20,44 +20,52 @@
 declare non_valid_id_message='Non-valid id'
 declare non_existent_id_message='Non-existent id'
 
-declare document_id=$1
 declare doc_source='pubmed'
 
-# If document ID is valid
-if grep -q "[1-9]" <<< $document_id; then
-    declare xml_response
-    declare title
-    declare abstract
+declare output='{}'
 
-    xml_response=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=$doc_source&retmode=xml&id=$document_id")
-    title=$(xmlstarlet sel -t -v //ArticleTitle <<< $xml_response)
-    abstract=$(xmlstarlet sel -t -v //AbstractText <<< $xml_response)
-fi
+for document_id in "$@"; do
 
-# If document ID is not valid
-if ! grep -q "[1-9]" <<< $document_id; then
-    output=$(jq -n --arg document_id "$document_id" \
-                   --arg doc_source "$doc_source" \
-                   --arg non_valid_id_message "$non_valid_id_message" \
-                   '{"doc_id": $document_id, "source": $doc_source, "error_message": $non_valid_id_message}')
-# If there is not title, then the ID is non-existent
-elif [[ -z $title ]]; then
-    output=$(jq -n --arg document_id "$document_id" \
-                   --arg doc_source "$doc_source" \
-                   --arg non_existent_id_message "$non_existent_id_message" \
-                   '{"doc_id": $document_id, "source": $doc_source, "error_message": $non_existent_id_message}')
-# If there is no abstract
-elif [[ -z $abstract ]]; then
-    output=$(jq -n --arg document_id "$document_id" \
-            --arg doc_source "$doc_source" \
-            --arg title "$title" \
-            '{"doc_id": $document_id, "source": $doc_source, "title": $title}')
-else
-    output=$(jq -n --arg document_id "$document_id" \
-                --arg doc_source "$doc_source" \
-                --arg title "$title" \
-                --arg abstract "$abstract" \
-                '{"doc_id": $document_id, "source": $doc_source, "title": $title, "abstract": $abstract}')
-fi
+  # If document ID is valid
+  if grep -q "[1-9]" <<< $document_id; then
+      declare xml_response
+      declare title
+      declare abstract
+
+      xml_response=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=$doc_source&retmode=xml&id=$document_id")
+      title=$(xmlstarlet sel -t -v //ArticleTitle <<< $xml_response)
+      abstract=$(xmlstarlet sel -t -v //AbstractText <<< $xml_response)
+  fi
+
+  # If document ID is not valid
+  if ! grep -q "[1-9]" <<< $document_id; then
+      output_section=$(jq -n --arg document_id "$document_id" \
+                       --arg doc_source "$doc_source" \
+                       --arg non_valid_id_message "$non_valid_id_message" \
+                       '{($document_id): {"source": $doc_source, "error_message": $non_valid_id_message}}')
+  # If there is not title, then the ID is non-existent
+  elif [[ -z $title ]]; then
+      output_section=$(jq -n --arg document_id "$document_id" \
+                       --arg doc_source "$doc_source" \
+                       --arg non_existent_id_message "$non_existent_id_message" \
+                       '{($document_id): {"source": $doc_source, "error_message": $non_existent_id_message}}')
+  # If there is no abstract
+  elif [[ -z $abstract ]]; then
+      output_section=$(jq -n --arg document_id "$document_id" \
+                      --arg doc_source "$doc_source" \
+                      --arg title "$title" \
+                      '{($document_id): {"source": $doc_source, "title": $title}}')
+  else
+      output_section=$(jq -n --arg document_id "$document_id" \
+                       --arg doc_source "$doc_source" \
+                       --arg title "$title" \
+                       --arg abstract "$abstract" \
+                       '{($document_id): {"source": $doc_source, "title": $title, "abstract": $abstract}}')
+  fi
+
+  # Merge with output being constructed
+  output=$(echo $output "$output_section" | jq -s '.[0] * .[1]')
+
+done
 
 echo "$output"
--- tests/test_external_services_scripts.py
+++ tests/test_external_services_scripts.py
@@ -7,7 +7,7 @@ import json
 
 class PubmedTests(unittest.TestCase):
 
-    def test_return_json_with_abstract_and_title(self):
+    def test_return_one_article_json_with_abstract_and_title(self):
 
         source = u'pubmed'
         doc_id = u'345345'
@@ -38,10 +38,11 @@ class PubmedTests(unittest.TestCase):
                          'disease: clinical disease and laboratory findings.')
 
         correct_dict = {}
-        correct_dict[u'source'] = source
-        correct_dict[u'doc_id'] = doc_id
-        correct_dict[u'abstract'] = correct_abstract
-        correct_dict[u'title'] = correct_title
+
+        correct_dict[doc_id] = {}
+        correct_dict[doc_id][u'source'] = source
+        correct_dict[doc_id][u'abstract'] = correct_abstract
+        correct_dict[doc_id][u'title'] = correct_title
 
         bash_command = 'bash external_services/pubmed.sh {}'.format(doc_id)
         result = subprocess.check_output(bash_command, shell=True)
@@ -50,7 +51,7 @@ class PubmedTests(unittest.TestCase):
 
         self.assertEqual(correct_dict, result_dict)
 
-    def test_return_json_only_with_title(self):
+    def test_return_one_article_json_only_with_title(self):
 
         source = u'pubmed'
         doc_id = u'435'
@@ -59,9 +60,10 @@ class PubmedTests(unittest.TestCase):
                          'psoas fiber bundles.')
 
         correct_dict = {}
-        correct_dict[u'source'] = source
-        correct_dict[u'doc_id'] = doc_id
-        correct_dict[u'title'] = correct_title
+
+        correct_dict[doc_id] = {}
+        correct_dict[doc_id][u'source'] = source
+        correct_dict[doc_id][u'title'] = correct_title
 
         bash_command = 'bash external_services/pubmed.sh {}'.format(doc_id)
         result = subprocess.check_output(bash_command, shell=True)
@@ -70,15 +72,16 @@ class PubmedTests(unittest.TestCase):
 
         self.assertEqual(correct_dict, result_dict)
 
-    def test_send_non_valid_doc_id(self):
+    def test_one_article_send_non_valid_doc_id(self):
 
         source = u'pubmed'
         doc_id = u'hjkf0s'
 
         correct_dict = {}
-        correct_dict[u'source'] = source
-        correct_dict[u'doc_id'] = doc_id
-        correct_dict[u'error_message'] = 'Non-valid id'
+
+        correct_dict[doc_id] = {}
+        correct_dict[doc_id][u'source'] = source
+        correct_dict[doc_id][u'error_message'] = 'Non-valid id'
 
         bash_command = 'bash external_services/pubmed.sh {}'.format(doc_id)
         result = subprocess.check_output(bash_command, shell=True)
@@ -87,17 +90,75 @@ class PubmedTests(unittest.TestCase):
 
         self.assertEqual(correct_dict, result_dict)
 
-    def test_send_id_non_existent(self):
+    def test_one_article_send_id_non_existent(self):
 
         source = u'pubmed'
         doc_id = u'43534253245345'
 
         correct_dict = {}
-        correct_dict[u'source'] = source
-        correct_dict[u'doc_id'] = doc_id
-        correct_dict[u'error_message'] = 'Non-existent id'
+
+        correct_dict[doc_id] = {}
+        correct_dict[doc_id][u'source'] = source
+        correct_dict[doc_id][u'error_message'] = 'Non-existent id'
 
         bash_command = 'bash external_services/pubmed.sh {}'.format(doc_id)
+        result = subprocess.check_output(bash_command, shell=True)
+
+        result_dict = json.loads(result)
+
+        self.assertEqual(correct_dict, result_dict)
+
+    def test_return_more_than_one_article_json(self):
+
+        correct_dict = {}
+
+        source_1 = u'pubmed'
+        doc_id_1 = u'345345'
+
+        correct_abstract_1 = (u'As is apparent from the length of this review, a '
+                              'multitude of laboratory investigations can be performed on the blood '
+                              'of patients with AIHA and CHD. Unfortunately, because of the '
+                              'considerable complexity of some of these tests, their significance '
+                              'is not always apparent to the physician who treats the patient. '
+                              'Communication gaps between the laboratory scientist and the physician '
+                              'at the bedside are bound to occur because of the high degree of '
+                              'specialization of both immunohematology and medical care. The '
+                              'purpose of this review has been to bridge the communication gap. '
+                              'The agents that cause AIHA and CHD are antibodies. '
+                              'Although they are often autoantibodies of complex specificity, '
+                              'usually reacting with all normal red cells, '
+                              'they nevertheless obey most of the rules explaining the action of '
+                              'alloantibodies that sometimes complicate transfusion therapy. '
+                              'By approaching AIHA and CHD as antibody-induced conditions, and by '
+                              'regarding autoantibodies as similar in their actions to '
+                              'alloantibodies, hopefully, physicians will appreciate the '
+                              'significance of the tests performed in the laboratory. '
+                              'For their part, the laboratory workers will be able not only to '
+                              'report test results but also to explain the findings. This review '
+                              'may aid in establishing the essential dialogue.')
+
+        correct_title_1 = (u'Autoimmune hemolytic anemia and cold hemagglutinin '
+                           'disease: clinical disease and laboratory findings.')
+
+        correct_dict[doc_id_1] = {}
+        correct_dict[doc_id_1][u'source'] = source_1
+        correct_dict[doc_id_1][u'abstract'] = correct_abstract_1
+        correct_dict[doc_id_1][u'title'] = correct_title_1
+
+        source_2 = u'pubmed'
+        doc_id_2 = u'435'
+
+        correct_title_2 = (u'Stability of myofibrillar EDTA-ATPase in rabbit '
+                           'psoas fiber bundles.')
+
+        correct_dict[doc_id_2] = {}
+        correct_dict[doc_id_2][u'source'] = source_2
+        correct_dict[doc_id_2][u'title'] = correct_title_2
+
+        bash_command = 'bash external_services/pubmed.sh {} {}'.format(
+            doc_id_1, doc_id_2
+        )
+
         result = subprocess.check_output(bash_command, shell=True)
 
         result_dict = json.loads(result)
@@ -107,7 +168,7 @@ class PubmedTests(unittest.TestCase):
 
 class PatentServerTests(unittest.TestCase):
 
-    def test_return_json_with_abstract_and_title(self):
+    def test_return__json_with_abstract_and_title(self):
 
         source = u'Patent server'
         doc_id = u'CN101371925B'
