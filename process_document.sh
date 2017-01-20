e5357b2a172870e900fddeffcab498a4a1d8f60a
AndreLamurias <alamurias@lasige.di.fc.ul.pt>
2017-01-20 10:07:15 +0000

    Process patent server documents

--- process_document.sh
+++ process_document.sh
@@ -4,10 +4,15 @@ declare docid=$1
 declare source=$2
 SAVEIFS=$IFS; IFS=$(echo -en "");
 if [ $source = "PUBMED" ]; then
-    declare title=$(./external_services/pubmed.sh $docid 2>/dev/null| jq '.title')
-    declare abstract=$(./external_services/pubmed.sh $docid 2>/dev/null| jq '.abstract')
-    declare titleresults=$(./get_entities.sh $docid "T" "$title" "ChEBI")
-    declare abstractresults=$(./get_entities.sh $docid "A" $abstract "ChEBI")
-    echo $titleresults $abstractresults
+    declare script=$(echo "pubmed")
+elif [ $source = "PATENT SERVER" ]; then
+    declare script=$(echo "patent_server")
 fi
+
+declare title=$(./external_services/$script.sh $docid 2>/dev/null| jq '.title')
+declare abstract=$(./external_services/$script.sh $docid 2>/dev/null| jq '.abstract')
+declare titleresults=$(./get_entities.sh $docid "T" "$title" "all_terms" 2>/dev/null)
+declare abstractresults=$(./get_entities.sh $docid "A" $abstract "all_terms" 2>/dev/null)
+echo $titleresults
+echo $abstractresults
 
