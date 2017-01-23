1492267686ef4867f85c6a838944d117142965af
AndreLamurias <alamurias@lasige.di.fc.ul.pt>
2017-01-23 17:44:14 +0000

    Loop task ids until they are all finished

--- ibelight.cgi
+++ ibelight.cgi
@@ -30,10 +30,12 @@ timestamp() {
 }
 
 echo $(timestamp) $POST_DATA >> request_log.txt
-declare key=$(echo "e2b88b40674b13e92fc6e41aa2a858910e3ac3b8")
+#declare key=$(echo "e2b88b40674b13e92fc6e41aa2a858910e3ac3b8")
+declare key=$(echo "ad05c12336cc9c137f8010051ad0c2cc8f99bdff")
 declare becalm_key=$(echo $POST_DATA | jq '.becalm_key')
 
-if [ $becalm_key = '"3deb66a13349fc7889549dfda065a3d8877ac04f"' ]; then
+#if [ $becalm_key = '"3deb66a13349fc7889549dfda065a3d8877ac04f"' ]; then
+if [ $becalm_key = '"10924ad95a9800e21087271f08b1e360c2acbdbd"' ]; then
     declare method=$(echo $POST_DATA | jq '.method')
     if [ $method = '"getState"' ]; then
         declare serverstatus=$(echo 'Running')
@@ -49,6 +51,7 @@ if [ $becalm_key = '"3deb66a13349fc7889549dfda065a3d8877ac04f"' ]; then
         declare response=$(echo '{"status": 200, "success": true, "key":"'$key'"}')
         echo $response
         # process request
+        #./process_request.sh "$POST_DATA" >> response_log.txt 2>&1 &       
         ./process_request.sh "$POST_DATA" >> response_log.txt 2>&1 &       
     fi
     
--- process_request.sh
+++ process_request.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 #set -x #debug
+ts -S 3 > /dev/null 2>&1  # set ts to run 3 parallel jobs
 
 declare POST_DATA=$1
 
-declare key=$(echo "e2b88b40674b13e92fc6e41aa2a858910e3ac3b8")
+declare key=$(echo "ad05c12336cc9c137f8010051ad0c2cc8f99bdff")
 declare cid=$(echo $POST_DATA | jq '.parameters.communication_id' | tr -d '"')
+
+timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S"
+}
+
 
 # get IDs of patent documents
 declare patentids=$(echo $POST_DATA | jq '.parameters.documents[] | select(.source == "PATENT SERVER") | .document_id' | tr -d '"')
@@ -17,23 +23,43 @@ declare pubmedids=$(echo $POST_DATA | jq '.parameters.documents[] | select(.sour
 declare pmcids=$(echo $POST_DATA | jq '.parameters.documents[] | select(.source == "PMC") | .document_id' | tr -d '"')
         #docs=$(echo "$docs" "$(./external_services/pmc.sh $pmcids 2>/dev/null | jq '.[] | .doc_id + (" T " + .title, " A " + .abstract)' | tr -d '"')")
 declare results=$(echo -e "DOCUMENT_ID\tSECTION\tINIT\tEND\tSCORE\tANNOTATED_TEXT\tTYPE\tDATABASE_ID\n")
+declare taskids=$(echo "")
+
 SAVEIFS=$IFS;
 IFS=$'\n'
+
+# start jobs for each document abstract and title
 for i in $docs
 do
     #echo $i
     IFS=$' '; arr=($i)
     text=$(echo ${arr[@]:2})
+    #declare task_results=$(./process_document.sh  ${arr[@]:0:2} "$text")
     # use ts
-    #TASKID=$(ts ./process_document.sh  ${arr[@]:0:2} "$text")
-    #declare task_results=$(ts -c $TASKID)
-    declare task_results=$(./process_document.sh  ${arr[@]:0:2} "$text")
-    results=$(echo -e "$results\n$task_results")
+    TASKID=$(ts ./process_document.sh  ${arr[@]:0:2} "$text")
+    taskids=$(echo "$taskids $TASKID")
 done
 IFS=$SAVEIFS
+
+# wait until every job associated with this request is finished
+while [ -n "$taskids" ]; do
+    new_taskids= # temporary list to store unfinished jobs 
+    for i in $taskids
+    do
+        # echo $(timestamp) $i $(ts -s $i) $taskids "/" $new_taskids >> response_log.txt 
+	if [ $(ts -s $i) != 'finished'  ]; then
+            declare new_taskids=$(echo "$new_taskids $i")
+        else    
+	    declare task_results=$(ts -c $i)
+	    results=$(echo -e "$results\n$task_results")
+        fi
+    done 
+    declare taskids=$(echo $new_taskids)
+done
+
 # echo $(ts)
-echo -e $results
+echo -e $(timestamp) $results >> response_log.txt
 # save annotations
 declare responseurl=$(echo 'http://www.becalm.eu/api/saveAnnotations/TSV?apikey='$key'&communicationId='$cid)
-echo -e $responseurl
-curl -X POST --data "$results" $responseurl --header "Content-Type:text/tab-separated-values"
+echo -e $(timestamp) $responseurl >> response_log.txt
+curl -X POST --data "$results" $responseurl --header "Content-Type:text/tab-separated-values" >> response_log.txt
