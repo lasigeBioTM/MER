9f3ada8796af42895682224469694a3308e04c02
LLCampos <luis1212@sapo.pt>
2017-01-24 18:24:44 +0000

    refactor and clean code

--- external_services/patent_server.sh
+++ external_services/patent_server.sh
@@ -2,72 +2,32 @@
 # Usage:
 # bash patent_server.sh [document_id1] [document_id2] [document_id3] (...)
 #
-# Some error messages will be outputted, but they do not affect the result
-# and can't quiet them.
 #
 # Output is in JSON and it is something like this:
 #
-# {
-#   "id1": {
-#     "doc_id": "id1",
-#     "doc_source": "document_provider1",
+# [
+#   {
+#     "externalId": "id1",
 #     "title": "title1",
-#     "abstract": "abstract1" (if available)
+#     "abstractText": "abstract1"
 #   },
-#   "id2": {
-#     "doc_id": "id2",
-#     "doc_source": "document_provider2",
+#   {
+#     "externalId": "id2",
 #     "title": "title2",
-#     "abstract": "abstract2" (if available)
+#     "abstractText": "abstract2"
 #   },
-#   "id3": {
-#     "doc_id": "id3",
-#     "doc_source": "document_provider3",
+#   {
+#     "externalId": "id3",
 #     "title": "title3",
-#     "abstract": "abstract3" (if available)
-#   }
-# }
+#     "abstractText": "abstract3"
+#   },
+# ]
 #
 
+declare json_response
+declare json_data
+json_data=$(echo -e "$@"| printf %s "$(cat)" | jq -R -s -c 'split(" ")')
+json_data='{"patents":'$json_data'}'
+json_response=$(curl -X POST --header "Content-Type:application/json" -d "$json_data"  -s http://193.147.85.10:8087/patentserver/json)
+echo "$json_response"
 
-declare doc_source="Patent server"
-
-declare output='{}'
-
-#for document_id in "$@"; do
-
-    declare json_response
-    declare title
-    declare abstract
-    declare json_data=$(echo -e "$@"| printf %s "$(cat)" | jq -R -s -c 'split(" ")')
-    json_data=$(echo '{"patents":'$json_data'}')
-    json_response=$(curl -X POST --header "Content-Type:application/json" -d $json_data  -s http://193.147.85.10:8087/patentserver/json)
-    echo $json_response
-    
-    #title=$(jq '.title' <<< $json_response)
-    #abstract=$(jq '.abstractText' <<< $json_response)
-
-    # Removes double quotes
-    #title=${title:1:-1}
-
-    # If abstract is available
-    #if [[ ! $abstract = 'null' ]]; then
-    #    abstract=${abstract:1:-1}
-
-    #    output_section=$(jq -n --arg document_id "$document_id" \
-    #                    --arg doc_source "$doc_source" \
-    #                    --arg title "$title" \
-    #                    --arg abstract "$abstract" \
-    #                    '{($document_id): {"doc_id": $document_id, "source": $doc_source, "title": $title, "abstract": $abstract}}')
-    #else
-    #    output_section=$(jq -n --arg document_id "$document_id" \
-    #                     --arg doc_source "$doc_source" \
-    #                     --arg title "$title" \
-    #                     '{($document_id): {"doc_id": $document_id, "source": $doc_source, "title": $title}}')
-    #fi
-
-    #output=$(echo $output "$output_section" | jq -s '.[0] * .[1]')
-
-#done
-
-#echo "$output"
