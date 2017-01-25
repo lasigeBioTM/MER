a541c7a74d7c01154c59e3fe62a741cb7fb5d42f
LLCampos <luis1212@sapo.pt>
2017-01-25 11:59:32 +0000

    add script to download all data files from google drive

---
+++ download_all_data_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd data
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sb0U2R0JCeEF3LVk' -O cells.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sTGNVUmFnanVDQkU' -O alpha-amylase.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sS1hKMXFpNXhfS0E' -O ChEBI.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sUG5WOXl2dzN4V00' -O ChEMBL.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sVVQ1UlJEYWxGZWc' -O chemicals.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6scnRXVGllRno3QUU' -O diseases.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sUzVydDNScXQ1MzA' -O genes.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sVVh0a3E5WndXbHM' -O proteins.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6scFNmZl9pMmktTVE' -O subcellular_structures.txt
+
+cd ..
