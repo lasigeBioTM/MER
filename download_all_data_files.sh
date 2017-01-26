875d01f81ed51f5169dc0a474a00fc179b27aa60
AndreLamurias <alamurias@lasige.di.fc.ul.pt>
2017-01-26 10:14:35 +0000

    Change name of entity lists

--- download_all_data_files.sh
+++ download_all_data_files.sh
@@ -6,10 +6,10 @@ wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sTGNVUmFnanVDQkU' -O alpha-amylase.txt
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sS1hKMXFpNXhfS0E' -O ChEBI.txt
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sUG5WOXl2dzN4V00' -O ChEMBL.txt
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sVVQ1UlJEYWxGZWc' -O chemicals.txt
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6scnRXVGllRno3QUU' -O diseases.txt
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sUzVydDNScXQ1MzA' -O genes.txt
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sVVh0a3E5WndXbHM' -O proteins.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sVVQ1UlJEYWxGZWc' -O CHEMICAL.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6scnRXVGllRno3QUU' -O DISEASE.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sUzVydDNScXQ1MzA' -O GENE.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6sVVh0a3E5WndXbHM' -O PROTEIN.txt
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B5R2YTHDeD6scFNmZl9pMmktTVE' -O subcellular_structures.txt
 
 cd ..
