092ce7fda771b4aaeddab9302c41825c41c99b22
AndreLamurias <alamurias@lasige.di.fc.ul.pt>
2017-01-27 10:53:12 +0000

    Abstract server requests

--- process_request.sh
+++ process_request.sh
@@ -11,14 +11,25 @@ export CID=$(echo $POST_DATA | jq '.parameters.communication_id' | tr -d '"')
 timestamp() {
   date +"%Y-%m-%d_%H:%M:%S:%3N"
 }
-
-echo $(timestamp) "get docs from patent server" >> response_log.txt
+declare docs
+echo $(timestamp) "get docs from sources" >> response_log.txt
 # get IDs of patent documents
 declare patentids=$(echo $POST_DATA | jq '.parameters.documents[] | select(.source == "PATENT SERVER") | .document_id' | tr -d '"')
 # get docid sectionid text lines
-#declare docs=$(./external_services/patent_server.sh $patentids 2>/dev/null | jq '.[] | .doc_id + (" T " + .title, " A " + .abstract)' | tr -d '"')
-declare docs=$(./external_services/patent_server.sh $patentids 2>/dev/null | jq '.[] | .externalId + (" T " + .title, " A " + .abstractText)' | tr -d '"')
+
+if [ ${#patentids} -ge 1 ]; then
+    docs=$docs\ $(./external_services/patent_server.sh $patentids 2>/dev/null | jq '.[] | .externalId + (" T " + .title, " A " + .abstractText)' | tr -d '"')
+fi
+
+declare abstractids=$(echo $POST_DATA | jq '.parameters.documents[] | select(.source == "ABSTRACT SERVER") | .document_id' | tr -d '"')
+#echo $(timestamp) $abstractids >> response_log.txt
+# get docid sectionid text lines
+if [ ${#abstractids} -ge 1 ]; then
+    docs=$docs\ $(./external_services/abstract_server.sh $abstractids 2>/dev/null | jq '.[] | .externalId + (" T " + .title, " A " + .text)' | tr -d '"')
+fi
+
 echo $(timestamp) "done"  >> response_log.txt
+
 #declare pubmedids=$(echo $POST_DATA | jq '.parameters.documents[] | select(.source == "PubMed") | .document_id' | tr -d '"')
 #        docs=$(echo "$docs" "$(./external_services/pubmed.sh $pubmedids 2>/dev/null | jq '.[] | .doc_id + (" T " + .title, " A " + .abstract)' | tr -d '"')")
 #declare pmcids=$(echo $POST_DATA | jq '.parameters.documents[] | select(.source == "PMC") | .document_id' | tr -d '"')
