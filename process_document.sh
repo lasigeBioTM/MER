5513e607136bed15b4a27e2e05e70795d01a51b0
AndreLamurias <alamurias@lasige.di.fc.ul.pt>
2017-01-31 17:24:17 +0000

    Select entity types to be processed

--- process_document.sh
+++ process_document.sh
@@ -4,7 +4,7 @@ declare docid=$1
 declare section=$2
 declare text=$3
 
-SAVEIFS=$IFS; IFS=$(echo -en "")
+#SAVEIFS=$IFS; IFS=$(echo -en "")
 
 # use remote machines
 #declare results=$(ssh -i $ssh_key centos@$chemical_ip 'cd ~/IBELight/; ./get_entities.sh '$docid $section '"'$text'"' 'CHEMICAL 2>/dev/null')
@@ -12,12 +12,10 @@ SAVEIFS=$IFS; IFS=$(echo -en "")
 #results=$results$'\n'$(ssh -i $ssh_key centos@$chemical_ip 'cd ~/IBELight/; ./get_entities.sh '$docid $section '"'$text'"' 'UNKNOWN 2>/dev/null')
 
 # use local machine
-declare results=$(./get_entities.sh $docid $section "$text" "CHEMICAL" 2>/dev/null)
-results=$results$'\n'$(./get_entities.sh $docid $section "$text" "PROTEIN" 2>/dev/null)
-results=$results$'\n'$(./get_entities.sh $docid $section "$text" "DISEASE" 2>/dev/null)
-results=$results$'\n'$(./get_entities.sh $docid $section "$text" "GENE" 2>/dev/null)
-results=$results$'\n'$(./get_entities.sh $docid $section "$text" "SUBCELLULAR_STRUCTURE" 2>/dev/null)
-results=$results$'\n'$(./get_entities.sh $docid $section "$text" "CELL_LINE_AND_CELL_TYPE" 2>/dev/null)
-results=$results$'\n'$(./get_entities.sh $docid $section "$text" "UNKNOWN" 2>/dev/null)
+declare results=
+for i in $TYPES 
+do
+    IFS=$(echo -en ""); results=$results$'\n'$(./get_entities.sh $docid $section "$text" "$i" 2>/dev/null)
+done
 echo $results
 
