8a73456ed8902e08387918e47b00200493e5d21a
LLCampos <luis1212@sapo.pt>
2017-03-08 18:48:00 +0000

    refactor: apply some bash good practises

--- get_entities.sh
+++ get_entities.sh
@@ -2,13 +2,11 @@
 
 # set -x #debug
 
-declare document_id=$1
-declare section=$2
-declare original_text=$3
-declare data_source=$4
+declare original_text=$1
+declare data_source=$2
 
 # Process text
-declare text=${3,,} # Make text lowercase so the system is case insensitive
+declare text=${1,,} # Make text lowercase so the system is case insensitive
 text=$(sed "s/[^[:alnum:][:space:]()]/./g" <<< "$text") # Replace special characters
 text=$(sed -e 's/[[:space:]()@]\+/ /g' <<< $text) # remove multiple whitespace
 text=$(sed -e 's/\.$//' -e 's/\. / /g' <<< $text) # remove full stops
@@ -17,11 +15,16 @@ text=$(tr ' ' '\n' <<< $text | grep -v -w -f stopwords.txt | tr '\n' ' ') # Remo
 text=$(sed -e 's/^ *//' -e 's/ *$//' <<< $text) # Remove leading and trailing whitespace
 
 # Separates all the words in the text by pipes
-declare piped_text=$(sed -e 's/ \+/|/g' <<< $text)
+declare piped_text
+piped_text=$(sed -e 's/ \+/|/g' <<< $text)
 
 # Creates all combinations of pairs of consecutive words in the text.
-declare piped_pair_text1=$(sed -e 's/\([^ ]\+ \+[^ ]\+\) /\1|/g' <<< $text" XXX" | sed 's/|[^|]*$//')
-declare piped_pair_text2=$(sed -e 's/\([^ ]\+ \+[^ ]\+\) /\1|/g' <<< "XXX "$text" XXX"| sed 's/^[^|]*|//' | sed 's/|[^|]*$//')
+declare piped_pair_text1
+piped_pair_text1=$(sed -e 's/\([^ ]\+ \+[^ ]\+\) /\1|/g' <<< $text" XXX" | sed 's/|[^|]*$//')
+
+declare piped_pair_text2
+piped_pair_text2=$(sed -e 's/\([^ ]\+ \+[^ ]\+\) /\1|/g' <<< "XXX $text XXX"| sed 's/^[^|]*|//' | sed 's/|[^|]*$//')
+
 declare piped_pair_text=$piped_pair_text1'|'$piped_pair_text2
 
 
@@ -35,15 +38,16 @@ get_matches_positions () {
 	do
 		matches=$(sed 's/\./\[^ \]/g' <<< $matches) # avoid mixing word1 and word2...
 		matching_text=$new_matching_text
-		local result=$(awk 'BEGIN {IGNORECASE = 1}
-			match($0,/'$matches'/){
+		local result
+		result=$(awk 'BEGIN {IGNORECASE = 1}
+			match($0,/'"$matches"'/){
 				if (substr($0, RSTART-1, 1) ~ "[^[:alnum:]@]" && substr($0, RSTART+RLENGTH, 1) ~ "[^[:alnum:]@]")
-						print RSTART-2 "\t" \
-							  RSTART-2+RLENGTH "\t" \
-							  substr($0, RSTART, RLENGTH)}' <<< " $matching_text ")
-		local match_hidden=$(awk 'BEGIN {IGNORECASE = 1}
-								match($0,/'$matches'/){print substr($0, RSTART, RLENGTH)}' <<< " $matching_text " | tr '[:alnum:]' '@')
-		new_matching_text=$(awk 'BEGIN {IGNORECASE = 1} {sub(/'$matches'/,"'$match_hidden'",$0)}1' <<< $matching_text)
+						print RSTART-2 "\t" RSTART-2+RLENGTH "\t" substr($0, RSTART, RLENGTH)}' <<< " $matching_text ")
+
+		local match_hidden
+		match_hidden=$(awk 'BEGIN {IGNORECASE = 1}
+					   match($0,/'"$matches"'/){print substr($0, RSTART, RLENGTH)}' <<< " $matching_text " | tr '[:alnum:]' '@')
+		new_matching_text=$(awk 'BEGIN {IGNORECASE = 1} {sub(/'"$matches"'/,"'"$match_hidden"'",$0)}1' <<< $matching_text)
 		if [ ${#result} -ge 2 ]; then
 			results=$results$'\n'$result
 		fi
@@ -56,9 +60,10 @@ get_entities_source_word1 () {
 	local labels=$1
 	get_entities_source_word1_result=''
 	if [ ${#piped_text} -ge 2 ]; then
-		local matches=$(egrep '^('$piped_text')$' $labels |  tr '\n' '|' | sed 's/|[[:space:]]*$//')
+		local matches
+		matches=$(egrep '^('"$piped_text"')$' "$labels" |  tr '\n' '|' | sed 's/|[[:space:]]*$//')
 		if [ ${#matches} -ge 2 ]; then
-			get_matches_positions $matches
+			get_matches_positions "$matches"
 			get_entities_source_word1_result=$get_matches_positions_result
 		fi
 	fi
@@ -69,9 +74,10 @@ get_entities_source_word2 () {
 	local labels=$1
 	get_entities_source_word2_result=''
 	if [ ${#piped_pair_text} -ge 2 ]; then
-		local matches=$(egrep '^('$piped_pair_text')$' $labels |  tr '\n' '|' | sed 's/|[[:space:]]*$//')
+		local matches
+		matches=$(egrep '^('"$piped_pair_text"')$' "$labels" |  tr '\n' '|' | sed 's/|[[:space:]]*$//')
 		if [ ${#matches} -ge 2 ]; then
-			get_matches_positions $matches
+			get_matches_positions "$matches"
 			get_entities_source_word2_result=$get_matches_positions_result
 		fi
 	fi
@@ -83,10 +89,12 @@ get_entities_source_words () {
 	local labels=$2
 	get_entities_source_words_result=''
 	if [ ${#piped_pair_text} -ge 2 ]; then
-		local matches=$(egrep '^('$piped_pair_text')$' $labels2 | egrep '[[:alpha:]]{5,}' | tr '\n' '|' | sed 's/|[[:space:]]*$//' )
+		local matches
+		matches=$(egrep '^('"$piped_pair_text"')$' "$labels2" | egrep '[[:alpha:]]{5,}' | tr '\n' '|' | sed 's/|[[:space:]]*$//' )
         if [ ${#matches} -ge 2 ]; then
-		    local fullmatches=$(egrep '^('$matches')' $labels |  tr '\n' '|' | sed 's/|[[:space:]]*$//')
-			get_matches_positions $fullmatches
+        	local fullmatches
+		    fullmatches=$(egrep '^('"$matches"')' "$labels" |  tr '\n' '|' | sed 's/|[[:space:]]*$//')
+			get_matches_positions "$fullmatches"
 			get_entities_source_words_result=$get_matches_positions_result
 		fi
 	fi
@@ -95,16 +103,19 @@ get_entities_source_words () {
 declare get_entities_source_words_result=''
 get_entities_source () {
 	local source=$1
-	# echo "== BEGIN SOURCE == $source =="
 	cd data/
 
-	SAVEIFS=$IFS; IFS=$(echo -en "");
+	IFS=$(echo -en "");
 
- 	local result1=$(get_entities_source_word1 $source\_word1.txt && echo $get_entities_source_word1_result &)
+	local result1
+	local result2
+	local result3
 
-	local result2=$(get_entities_source_word2 $source\_word2.txt && echo $get_entities_source_word2_result &)
+ 	result1=$(get_entities_source_word1 "$source"_word1.txt && echo "$get_entities_source_word1_result" &)
 
-	local result3=$(get_entities_source_words $source\_words2.txt $source\_words.txt && echo $get_entities_source_words_result &)
+	result2=$(get_entities_source_word2 "$source"_word2.txt && echo "$get_entities_source_word2_result" &)
+
+	result3=$(get_entities_source_words "$source"_words2.txt "$source"_words.txt && echo $get_entities_source_words_result &)
 
 	wait
 	cd ..
@@ -117,8 +128,7 @@ get_entities_source () {
 	local result=$result1$'\n'$result2$'\n'$result3
 	result=$(sed '{/^$/d}' <<< $result) # remove empty lines
 
-	echo $result
-	#echo "== END SOURCE =="
+	echo "$result"
 	}
 
-get_entities_source $data_source
+get_entities_source "$data_source"
