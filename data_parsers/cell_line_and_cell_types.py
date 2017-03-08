4ed4d989e4ef5860c9acc3dc8d4756ee315c8282
LLCampos <luis1212@sapo.pt>
2017-03-08 18:11:07 +0000

    create simple version of IBELight

--- data_parsers/cell_line_and_cell_types.py
+++
@@ -1,9 +0,0 @@
-from lxml import etree
-
-with open('cellosaurus.xml') as f:
-    cell_xml = etree.parse(f)
-
-names = cell_xml.findall('//name')
-
-with open('../../data/cellosaurus.txt', 'w') as f:
-    map(lambda name: f.write(name.text + '\n'), names)
--- data_parsers/tissue_and_organ_process_query_results.py
+++
@@ -1,9 +0,0 @@
-with open('tissue_and_organ_query_result.txt') as f:
-    terms = f.readlines()[3:-1]
-
-terms = map(lambda term: term.strip(), terms)
-terms = map(lambda term: term[1:-1].strip(), terms)
-terms = map(lambda term: term[1:-1], terms)
-
-with open('../../data/TISSUE_AND_ORGAN.txt', 'w') as f:
-    map(lambda term: f.write(term + '\n'), terms)
