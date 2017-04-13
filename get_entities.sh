372c9e272d25e224560074ec99da1f201da18032
LLCampos <luis1212@sapo.pt>
2017-04-13 19:09:26 +0100

    Update get_entitiies.sh with what was in master

--- get_entities.sh
+++ get_entities.sh
@@ -5,8 +5,15 @@
 declare original_text=$(tr '\n\r' ' ' <<< $1)
 declare data_source=$2
 
-# Process text
-declare text=${1,,} # Make text lowercase so the system is case insensitive
+# Check the given parameters
+if [[ -z $original_text || -z $data_source ]]; then 
+	echo "Usage: get_entities.sh <text> <lexicon>"
+	echo "Check the examples in the test.sh file"
+	exit
+fi
+
+# Pre-process the input text
+declare text=$(tr '[:upper:]' '[:lower:]' <<< "$original_text") # Make text lowercase so the system is case insensitive
 text=$(sed "s/[^[:alnum:][:space:]()]/./g" <<< "$text") # Replace special characters
 text=$(sed -e 's/[[:space:]()@]\+/ /g' <<< $text) # remove multiple whitespace
 text=$(sed -e 's/\.$//' -e 's/\. / /g' <<< $text) # remove full stops
@@ -18,32 +25,37 @@ text=$(sed -e 's/^ *//' -e 's/ *$//' <<< $text) # Remove leading and trailing wh
 declare piped_text
 piped_text=$(sed -e 's/ \+/|/g' <<< $text)
 
-# Creates all combinations of pairs of consecutive words in the text.
+# Creates all combinations of pairs of consecutive words in the text, staring at the first word 
 declare piped_pair_text1
 piped_pair_text1=$(sed -e 's/\([^ ]\+ \+[^ ]\+\) /\1|/g' <<< $text" XXX" | sed 's/|[^|]*$//')
 
+# Creates all combinations of pairs of consecutive words in the text, staring at the second word
 declare piped_pair_text2
 piped_pair_text2=$(sed -e 's/\([^ ]\+ \+[^ ]\+\) /\1|/g' <<< "XXX $text XXX"| sed 's/^[^|]*|//' | sed 's/|[^|]*$//')
 
+# Joins both previous combinations of pair of words
 declare piped_pair_text=$piped_pair_text1'|'$piped_pair_text2
 
-
+# Function that finds the start and end position of each given matched term 
 declare get_matches_positions_result=''
 get_matches_positions () {
 	local matches=$1
 	local results=''
 	local matching_text=' '
 	local new_matching_text=$original_text
+	
+	# While there are changes in the new_matching_text 
 	while [ "$new_matching_text" != "$matching_text" ];
 	do
 		matches=$(sed 's/\./\[^ \]/g' <<< $matches) # avoid mixing word1 and word2...
 		matching_text=$new_matching_text
+		# Finds the start and end position of the first match
 		local result
 		result=$(awk 'BEGIN {IGNORECASE = 1}
 			match($0,/'"$matches"'/){
 				if (substr($0, RSTART-1, 1) ~ "[^[:alnum:]@-]" && substr($0, RSTART+RLENGTH, 1) ~ "[^[:alnum:]@-]")
 						print RSTART-2 "\t" RSTART-2+RLENGTH "\t" substr($0, RSTART, RLENGTH)}' <<< " $matching_text ")
-
+		# Masks the match in the matching text to avoid full overlapping matches  
 		local match_hidden
 		match_hidden=$(awk 'BEGIN {IGNORECASE = 1}
 					   match($0,/'"$matches"'/){print substr($0, RSTART, RLENGTH)}' <<< " $matching_text " | tr '[:alnum:]' '@')
@@ -55,6 +67,7 @@ get_matches_positions () {
 	get_matches_positions_result=$results;
 }
 
+# Function that matches the one-word pattern (piped_text) in the one-word file of the lexicon (labels)  
 declare get_entities_source_word1_result=''
 get_entities_source_word1 () {
 	local labels=$1
@@ -69,6 +82,7 @@ get_entities_source_word1 () {
 	fi
 }
 
+# Function that matches the two-word pattern (piped_pair_text) in the two-word file of the lexicon (labels)  
 declare get_entities_source_word2_result=''
 get_entities_source_word2 () {
 	local labels=$1
@@ -83,16 +97,19 @@ get_entities_source_word2 () {
 	fi
 }
 
+# Function that matches the two-word pattern (piped_pair_text) in the two-first-words (labels2) and more-words (labels) files of the lexicon   
 declare get_entities_source_words_result=''
 get_entities_source_words () {
 	local labels2=$1
 	local labels=$2
 	get_entities_source_words_result=''
 	if [ ${#piped_pair_text} -ge 2 ]; then
+		# finds the two-first-word matches  
 		local matches
 		matches=$(egrep '^('"$piped_pair_text"')$' "$labels2" | egrep '[[:alpha:]]{5,}' | tr '\n' '|' | sed 's/|[[:space:]]*$//' )
         if [ ${#matches} -ge 2 ]; then
-        	local fullmatches
+            # finds the more-words matches based on the previous matches
+			local fullmatches
 		    fullmatches=$(egrep '^('"$matches"')' "$labels" |  tr '\n' '|' | sed 's/|[[:space:]]*$//')
 			get_matches_positions "$fullmatches"
 			get_entities_source_words_result=$get_matches_positions_result
@@ -100,6 +117,7 @@ get_entities_source_words () {
 	fi
 }
 
+# Function that launches one job for each of the 3 types of matches
 declare get_entities_source_words_result=''
 get_entities_source () {
 	local source=$1
@@ -125,6 +143,7 @@ get_entities_source () {
 		return
 	fi
 
+	# combine the results from the 3 types of matches
 	local result=$result1$'\n'$result2$'\n'$result3
 	result=$(sed '{/^$/d}' <<< $result) # remove empty lines
 
