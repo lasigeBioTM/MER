2323268255f59622d90171ad8428b26d4f2d4ef4
AndreLamurias <andrelamurias@gmail.com>
2017-05-08 16:05:17 +0100

    fix test.sh permissions

--- test.sh
+++ test.sh
@@ -9,10 +9,8 @@ correct_result='0	9	α-maltose
 
 result=$(./get_entities.sh "α-maltose and nicotinic acid was found, but not nicotinic acid D-ribonucleotide" lexicon)
 
-if diff -y <(echo $correct_result) <(echo $result); then 
+if diff -y <(echo $correct_result) <(echo $result); then
  echo "Great you are ready to go!"
 else
  echo "Sorry, something is not working well!"
 fi
-
-####
