a80a8d14a6abb09e8d983051e44fd3d7aee75a9e
Andre Lamurias <alamurias@lasige.di.fc.ul.pt>
2019-03-04 17:43:29 +0000

    add produce_data_files to test.sh

--- test.sh
+++ test.sh
@@ -7,6 +7,8 @@ correct_result='0	9	α-maltose	http://purl.obolibrary.org/obo/CHEBI_18167
 48	62	nicotinic acid	http://purl.obolibrary.org/obo/CHEBI_15940
 48	79	nicotinic acid D-ribonucleotide	http://purl.obolibrary.org/obo/CHEBI_15763'
 
+
+(cd data; ../produce_data_files.sh lexicon.txt)
 result=$(./get_entities.sh "α-maltose and nicotinic acid was found, but not nicotinic acid D-ribonucleotide" lexicon)
 
 if diff -y <(echo $correct_result) <(echo $result); then
